b596baacca7993f44e68ab22a3f543d53956df4d
1015\Jason DiMedio <jason.dimedio@houstonllp.com>
2019-03-06 16:31:58 -0500

    no message

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var mysql = require('./dbcon.js');
 
 var req = {};
-req.street_name = "NE 7th Ave";
-req.cross_street_1_name = "cross street 1";
-req.cross_street_2_name = "cross street 2";
+req.street_name = "NE Knott St";
+req.cross_street_1_name = "NE 7th Ave";
+req.cross_street_2_name = "NE 8th Ave";
 
 var res = {
   send : function(input) {
@@ -11,10 +11,10 @@ var res = {
   }
 };
 
+var next = function(){console.log("error.\n"};
 
 
-
-function get_street_id(req, res) {
+function get_street_id(req, res, next) {
 	mysql.pool.query('SELECT id FROM street WHERE name=?', [req.street_name], function(err, rows, fields){
 		if(err){
 			next(err);
@@ -25,7 +25,7 @@ function get_street_id(req, res) {
 	});
 }
 
-function get_availability(req, res) {
+function get_availability(req, res, next) {
 	mysql.pool.query('SELECT id, name FROM street WHERE name=? OR name=? OR name=?', [req.street_name, req.cross_street_1_name, req.cross_street_2_name], function(err, rows, fields){
 		if(err){
 			next(err);
