6caaf3a8791d9e954531d1ab70e3db51297fd4a1
1015\Jason DiMedio <jason.dimedio@houstonllp.com>
2019-03-06 15:21:40 -0500

    d

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ var street_name = "NE 7th Ave";
 var cross_street_1_name = "cross street 1";
 var cross_street_2_name = "cross street 2";
 
+function get_street_id(name, mysql) {
+	mysql.pool.query('SELECT id FROM street WHERE name=?', name, function(err, rows, fields){
+		if(err){
+			next(err);
+			return;
+		}
+		else		
+			return rows;
+	});
+}
 
-mysql.pool.query('SELECT id FROM street WHERE name=?', street_name, function(err, rows, fields){
-	if(err){
-		next(err);
-		   return;
-	}
-	else		
-		console.log(JSON.stringify(rows));
-});
+var result = get_street_id(name,mysql);
+
+console.log(JSON.stringify(result));
