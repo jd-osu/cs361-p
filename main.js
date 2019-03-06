bade4d9526c3f5b347378349eb309c3df4fbb3a9
1015\Jason DiMedio <jason.dimedio@houstonllp.com>
2019-03-06 14:56:36 -0500

    d

--- main.js
+++ main.js
@@ -2,8 +2,13 @@
 
 var mysql = require('./dbcon.js');
 
-mysql.pool.query('SELECT * FROM street', function(err, rows, fields) {
+mysql.pool.query('SELECT id, street_id, cross_street_1, cross_street_2 FROM sidewalk', function(err, rows, fields){
+	if(err){
+		next(err);
+		   return;
+	}
+	else		
+		res.send(JSON.stringify(rows));
+});
 
-}
-
-console.log("test");
+console.log("\n");
