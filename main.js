74e98463d456ca1286eca99092779d5a459d3812
1015\Jason DiMedio <jason.dimedio@houstonllp.com>
2019-03-06 15:55:22 -0500

    d

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var mysql = require('./dbcon.js');
 
-var req;
+var req = {};
 req.street_name = "NE 7th Ave";
 req.cross_street_1_name = "cross street 1";
 req.cross_street_2_name = "cross street 2";
