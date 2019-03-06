929b2769e7359d3e77850bceb65cdb72090d7227
1015\Jason DiMedio <jason.dimedio@houstonllp.com>
2019-03-06 16:27:12 -0500

    d

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ var res = {
   }
 };
 
+
+
+
 function get_street_id(req, res) {
 	mysql.pool.query('SELECT id FROM street WHERE name=?', [req.street_name], function(err, rows, fields){
 		if(err){
@@ -22,5 +25,18 @@ function get_street_id(req, res) {
 	});
 }
 
+function get_availability(req, res) {
+	mysql.pool.query('SELECT id, name FROM street WHERE name=? OR name=? OR name=?', [req.street_name, req.cross_street_1_name, req.cross_street_2_name], function(err, rows, fields){
+		if(err){
+			next(err);
+			return;
+		}
+		else		
+			res.send(JSON.stringify(rows));
+	});
+}
 
-get_street_id(req, res);
+
+
+//get_street_id(req, res);
+get_availability(req, res);
